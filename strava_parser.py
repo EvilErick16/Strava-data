cbb3b9a12d3a84cd7264f6a02eee4b74b637781c
Erick Juarez <juarez.erick16@csu.fullerton.edu>
2019-10-13 17:38:15 -0700

    Learning Numpy

--- strava_parser.py
+++ strava_parser.py
@@ -10,7 +10,36 @@ reader = csv.reader(csv_file)
 """
 Learning how to use matplotlib here's an examnple  
 """
+dateAndTime = []
+activityType = []
+elapsedTime = []
+distance = []
+count = 0
+for x in reader:
+    if count != 0:
+        dateAndTime.append(x[1])
+        activityType.append(x[3])
+        elapsedTime.append(x[5])
+        distance.append(x[6])
+    count = 1
 
+
+numberOfActivities = len(dateAndTime)
+ind = np.arange(numberOfActivities)
+width = 0.5
+p1 = plt.bar(ind, elapsedTime, width)
+p2 = plt.bar(ind, 1, width, bottom=elapsedTime)
+
+plt.ylabel("Elapsed time")
+plt.title("Time spent running")
+plt.xticks(ind, ("22 Feb", "20 Feb", "23 Feb", "23 Feb", "23 Feb", "25 Feb", "25 Feb"))
+plt.yticks(np.arange(0, 25, 1))
+#plt.legend(p1[0], "Seconds")
+
+
+plt.show()
+
+"""
 N = 5
 menMeans = (20, 35, 30, 35, 27)
 womenMeans = (25, 32, 34, 20, 25)
@@ -30,3 +59,4 @@ plt.yticks(np.arange(0, 81, 10))
 plt.legend((p1[0], p2[0]), ('Men', 'Women'))
 
 plt.show()
+"""
